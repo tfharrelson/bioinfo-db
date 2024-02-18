8aa3f55bff4402aefaf27fccd99372e901cf3ff4
Tommy Harrelson <tommy.harrelson@emeraldcloudlab.com>
2024-02-18 09:26:14 -0800

    wip: in the middle of something

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.20
 
 require github.com/gorilla/mux v1.8.1
 
-require github.com/gorilla/schema v1.2.1 
+require (
+	github.com/gorilla/schema v1.2.1
+	github.com/lib/pq v1.10.9
+)
--- internal/app/server.go
+++ internal/app/server.go
@@ -1,13 +1,19 @@
 package app
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+
+	_ "github.com/lib/pq"
 )
 
 type App struct {
 	Router *mux.Router
+	db *sql.DB
 }
 
 func (a *App) Initialize() {
@@ -20,8 +26,17 @@ func (a *App) Initialize() {
 
 	// return the router
 	a.Router = router
+
+	// start up the DB connection
+	// TODO: break apart this bad boi and do some error checking to make sure the env vars are set
+	connString := "postgres://" + os.Getenv("DBUSER") + ":" + os.Getenv("DBPASSWORD") + "@localhost/" + os.Getenv("DBNAME") + "?sslmode=disable"
+	db, err := sql.Open(connString, "postgres")
+	if err != nil {
+		panic("failed db connection")
+	}
+	a.db = db
 }
 
 func (a *App) Run() {
-	http.ListenAndServe(":8080", a.Router)
+	log.Fatal(http.ListenAndServe(":8080", a.Router))
 }
